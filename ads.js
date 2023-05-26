9b68a5c9c722964a2f159a01dc4312cd2c0dec4a
DCCS TEAM <128686785+Dccs-team@users.noreply.github.com>
2023-05-26 17:14:09 +0600

    Create ads.js

---
+++ ads.js
@@ -0,0 +1,17 @@
+<script type="text/javascript">
+
+	atOptions = {		'key' : '33c5d34cbab13eb654da3f3572cbf258',
+
+		'format' : 'iframe',
+
+		'height' : 250,
+
+		'width' : 300,
+
+		'params' : {}
+
+	};
+
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.profitabledisplaynetwork.com/33c5d34cbab13eb654da3f3572cbf258/invoke.js"></scr' + 'ipt>');
+
+</script>
