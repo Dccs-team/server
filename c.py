c4ab00cef619b4a08699ed2cab8e81a8da914634
DCCS TEAM <128686785+Dccs-team@users.noreply.github.com>
2023-08-22 20:34:51 +0600

    Update c.py

--- c.py
+++ c.py
@@ -2,9 +2,12 @@ import requests
 from concurrent.futures import ThreadPoolExecutor
 
 def make_request(url):
-    with requests.Session() as ses:
-        response = ses.get(url)
-        print(response.text)
+    try:
+        response = requests.get(url)
+        response.raise_for_status()  # Raise an exception for bad responses
+        print("Request to", url, "is done")
+    except requests.RequestException as e:
+        print("Request to", url, "failed:", e)
 
 if __name__ == "__main__":
     urls = ['https://camo.githubusercontent.com/26a5a007ec201d8120179ce042c538274f11c0da3c6909c83eaf79547b2186a1/68747470733a2f2f6b6f6d617265762e636f6d2f67687076632f3f757365726e616d653d646363732d7465616d' for _ in range(10000)]
