7757cf4156d15bfa4f3468411668a40fdea5874e
DCCS TEAM <128686785+Dccs-team@users.noreply.github.com>
2023-08-22 20:29:04 +0600

    Update c.py

--- c.py
+++ c.py
@@ -4,10 +4,10 @@ from concurrent.futures import ThreadPoolExecutor
 def make_request(url):
     with requests.Session() as ses:
         response = ses.get(url)
-        print('Done')
+        print(response.text)
 
 if __name__ == "__main__":
-    urls = ['https://camo.githubusercontent.com/5fe4c339179e986bbc91d54cbda95cc20472a740bc07d1ce4c7ebd1a8589cfac' for _ in range(10000)]
+    urls = ['https://camo.githubusercontent.com/26a5a007ec201d8120179ce042c538274f11c0da3c6909c83eaf79547b2186a1/68747470733a2f2f6b6f6d617265762e636f6d2f67687076632f3f757365726e616d653d646363732d7465616d' for _ in range(10000)]
 
     with ThreadPoolExecutor(max_workers=10) as executor:
         executor.map(make_request, urls)
