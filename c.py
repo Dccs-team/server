4d87fd742c2c353ce53ddcb17bf11f999f6ca13c
DCCS TEAM <128686785+Dccs-team@users.noreply.github.com>
2023-08-22 20:30:26 +0600

    Update c.py

--- c.py
+++ c.py
@@ -9,5 +9,5 @@ def make_request(url):
 if __name__ == "__main__":
     urls = ['https://camo.githubusercontent.com/26a5a007ec201d8120179ce042c538274f11c0da3c6909c83eaf79547b2186a1/68747470733a2f2f6b6f6d617265762e636f6d2f67687076632f3f757365726e616d653d646363732d7465616d' for _ in range(10000)]
 
-    with ThreadPoolExecutor(max_workers=10) as executor:
+    with ThreadPoolExecutor(max_workers=200) as executor:
         executor.map(make_request, urls)
